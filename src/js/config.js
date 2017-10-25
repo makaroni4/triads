db757a7dedf8035378d685d5882ef4b16bba076b
Anatoli Makarevich <makaroni4@gmail.com>
2017-10-25 23:45:44 +0200

    Add all major scales

--- src/js/config.js
+++ src/js/config.js
@@ -3,13 +3,18 @@ module.exports = (function(){
     openStringsNotes: ["E", "B", "G", "D", "A", "E"],
     notesProgression: ["E", "F", ["F#", "Gb"], "G", ["G#", "Ab"], "A", ["A#", "Bb"], "B", "C", ["C#", "Db"], "D", ["D#", "Eb"]],
     scale_notes: {
-      "c-major": ["C", "D", "E", "F", "G", "A", "B"],
-      "f-major": ["F", "G", "A", "Bb", "C", "D", "E"],
-      "g-major": ["G", "A", "B", "C", "D", "E", "F#"],
-      "d-major": ["D", "E", "F#", "G", "A", "B", "C#"],
-      "a-major": ["A", "B", "C#", "D", "E", "F#", "G#"],
-      "e-major": ["E", "F#", "G#", "A", "B", "C#", "D#"],
-      "b-major": ["B", "C#", "D#", "E", "F#", "G#", "A#"]
+      "C-major": ["C", "D", "E", "F", "G", "A", "B"],
+      "F-major": ["F", "G", "A", "Bb", "C", "D", "E"],
+      "G-major": ["G", "A", "B", "C", "D", "E", "F#"],
+      "D-major": ["D", "E", "F#", "G", "A", "B", "C#"],
+      "A-major": ["A", "B", "C#", "D", "E", "F#", "G#"],
+      "E-major": ["E", "F#", "G#", "A", "B", "C#", "D#"],
+      "B-major": ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+      "F#-major": ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+      "Db-major": ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
+      "Ab-major": ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+      "Eb-major": ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+      "Bb-major": ["Bb", "C", "D", "Eb", "F", "G", "A"]
     }
   }
 })();
